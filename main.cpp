057e46049faa02542467f22c44cb95e1eeb688cd
MisterE1234 <MisterE1234@outlook.com>
2025-11-29 13:51:31 -0800

    3rd Commit

--- main.cpp
+++ main.cpp
@@ -6,14 +6,32 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
-int SZ_CODES = 20000;
+int SZ_CODES = 20000; bool debug = true;
 
-void addNode()
+void addNode(string[], IntBinaryTree&);
 
 int main() {
+    string tempString;
     string randString[SZ_CODES];
     IntBinaryTree stringTree();
-    
+
+    //opening the codes for the array
+    ifstream iFile("codes.txt");
+
+    //using a for loop to read the file to the array:
+    for(int i = 0; i < SZ_CODES; i++){
+
+        getline(iFile, randString[i]);
+
+    }
+
+    iFile.close();
+
+    if(debug){
+        for(int i = 0; i < 10; i++){
+            cout << randString[i] << endl;
+        }
+    }
 
 
     return 0;
